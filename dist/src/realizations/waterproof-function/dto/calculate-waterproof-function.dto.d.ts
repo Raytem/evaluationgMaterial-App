f317c7accaa12a0122d28631ade413bce534871d
Daniil <Danik1320@yandex.by>
2023-10-09 20:32:25 +0300

    build

--- dist/src/realizations/waterproof-function/dto/calculate-waterproof-function.dto.d.ts
+++ dist/src/realizations/waterproof-function/dto/calculate-waterproof-function.dto.d.ts
@@ -1,5 +1,5 @@
 import { WaterproofFunctionEntity } from '../entities/waterproof-function.entity';
-declare const CalculateWaterproofFunctionDto_base: import("@nestjs/common").Type<Omit<WaterproofFunctionEntity, "comment" | "avgWeightedEstimate" | "id" | "waterproofRealizationCriteria_calculated" | "dynamicWaterproofCriteria_calculated" | "materialBlottingPressure_relativeValuation" | "waterproof_relativeValuation" | "materialBlottingTime_relativeValuation" | "waterproofRealizationCriteria_relativeValuation" | "dynamicWaterproofCriteria_relativeValuation">>;
+declare const CalculateWaterproofFunctionDto_base: import("@nestjs/common").Type<Omit<WaterproofFunctionEntity, "id" | "comment" | "avgWeightedEstimate" | "materialBlottingPressure_relativeValuation" | "waterproof_relativeValuation" | "materialBlottingTime_relativeValuation" | "waterproofRealizationCriteria_calculated" | "waterproofRealizationCriteria_relativeValuation" | "dynamicWaterproofCriteria_calculated" | "dynamicWaterproofCriteria_relativeValuation">>;
 export declare class CalculateWaterproofFunctionDto extends CalculateWaterproofFunctionDto_base {
 }
 export {};

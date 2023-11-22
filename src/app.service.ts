16995f1930852403444031e693c34cf806e6da0f
Daniil <Danik1320@yandex.by>
2023-11-22 14:42:51 +0300

    test changes

--- src/app.service.ts
+++ src/app.service.ts
@@ -3,6 +3,6 @@ import { Injectable } from '@nestjs/common';
 @Injectable()
 export class AppService {
   getHello(): string {
-    return 'Evaluating material app';
+    return 'Welcome to Evaluating material app!';
   }
 }

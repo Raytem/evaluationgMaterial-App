7057ba0b8f6c20d0865fa9ac44eccae19722ff40
Daniil_Rummo <Danik1320@yandex.by>
2024-02-13 02:16:21 +0300

    small fixes

--- src/utils/getWebContentLink.ts
+++ src/utils/getWebContentLink.ts
@@ -2,10 +2,12 @@ import { NodeEnv } from 'src/enums/node-env';
 
 export const getWebContentLink = (fileName: string, materialFolder) => {
   let serverUrl;
-  if (process.env.NODE_ENV === NodeEnv.DEVELOPMENT) {
-    serverUrl = process.env.APP_URL;
+
+  if (process.env.NODE_ENV === NodeEnv.PRODUCTION) {
+    serverUrl = process.env.DOMAIN_URL;
   } else {
-    serverUrl = process.env.APP_PROXY_URL;
+    serverUrl = process.env.APP_URL;
   }
+
   return `${serverUrl}/${process.env.FILE_STATIC_DIR_NAME}/${process.env.FILE_MATERIAL_IMAGES_DIR_NAME}/${materialFolder}/${fileName}`;
 };

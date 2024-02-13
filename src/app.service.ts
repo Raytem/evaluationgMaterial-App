15542a96b2556594faf3b6ec76bdefc53f566113
Daniil_Rummo <Danik1320@yandex.by>
2024-02-13 13:21:32 +0300

    updated hello route

--- src/app.service.ts
+++ src/app.service.ts
@@ -3,6 +3,6 @@ import { Injectable } from '@nestjs/common';
 @Injectable()
 export class AppService {
   getHello(): string {
-    return 'Welcome to Evaluating material app!';
+    return 'Welcome to KOMFORT app!';
   }
 }
